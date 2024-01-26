10ea35ed4120a4ff5f1e8de220a3262635e128a4
KnGod666 <ianrvr@estudiantes.uci.cu>
2024-01-26 12:24:04 -0500

    basic character movement

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,19 +1,22 @@
 extends Node2D
 @onready var raycast = $RayCast2D
-var speed = 0
+var speed = Vector2(0,0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_viewport().warp_mouse(position)
-	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	#Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	pass # Replace with function body.
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		var e = event as InputEventMouseMotion
-		print(e.velocity)
+		speed = e.velocity
 		pass
 	pass
 
+func _physics_process(delta):
+	position += speed*delta
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
