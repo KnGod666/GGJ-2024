bb93e64f15c92bcecc63797d28a92664a28cf00a
Kosmo00 <vladiotero19101916@gmail.com>
2024-01-27 16:41:15 -0500

    Solving dialog bar problems

--- scenes/Maps/Level1/lvl1.cs
+++ scenes/Maps/Level1/lvl1.cs
@@ -2,32 +2,17 @@ using Godot;
 
 public partial class lvl1 : Control
 {
-		private readonly string ACTION = "accion";
-	private DialogBar dialog_box;
+	private DialogBar dialog_bar;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		
-		dialog_box = GetNode<DialogBar>("DialogBar");
-		dialog_box.ShowMessages("muerte4");
+		dialog_bar = GetNode<DialogBar>("DialogBar");
+		dialog_bar.ShowMessages("jose", "muerte1");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
-	    public override void _Input(InputEvent @event)
-    {
-        base._Input(@event);
-		if(@event.IsActionPressed(ACTION)){
-			if(dialog_box.GetHasNext())
-			{
-				if(!dialog_box.NextMessage())
-				{
-					dialog_box.Hide();
-				}
-
-			}
-		}
-    }
 }
