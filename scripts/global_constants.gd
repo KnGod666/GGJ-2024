5aa6a07165569f7a806928cd7d5009a24f2fca95
HollowDude <madafakn@gmail.com>
2024-01-26 22:05:36 -0500

    Los globals

--- scripts/global_constants.gd
+++ scripts/global_constants.gd
@@ -1,6 +1,10 @@
 extends Node
 
 const maps_ids = {0:"res://scenes/Maps/begin.tscn",1:"res://scenes/Maps/hollow_lvl.tscn",2:"res://scenes/Maps/Kn_map.tscn"}
+const Pos_Hollow = Vector2(3510, 2171)
+const Pos_Kn = Vector2(-570, 0)
+const Pos_Begin = Vector2(300, 400)
+const Pos_JL = Vector2(0, 0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
