2ce7a070c840e69347aaa774f6e0ef6d17b8f4f5
Jhulian Garcia <jhpiano9731@gmail.com>
2024-01-28 00:03:01 -0500

    [ADD] mylevel :D

--- scenes/Maps/jhlvl/jhlvl.gd
+++ scenes/Maps/jhlvl/jhlvl.gd
@@ -5,7 +5,7 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-func _input(event):
+func _input(_event):
 	if Input.is_key_pressed(KEY_S):
 		$Player.stopped = not $Player.stopped
 	pass
@@ -13,3 +13,5 @@ func _input(event):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
