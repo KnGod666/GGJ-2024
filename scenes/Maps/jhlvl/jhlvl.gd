119a3f37d5cd9550bb0895b540f1e298e78f86b3
Jhulian Garcia <jhpiano9731@gmail.com>
2024-01-27 23:22:32 -0500

    [ADD] script for pausing the player's movement

--- scenes/Maps/jhlvl/jhlvl.gd
+++ scenes/Maps/jhlvl/jhlvl.gd
@@ -6,9 +6,10 @@ func _ready():
 	pass # Replace with function body.
 
 func _input(event):
-	$Player.
+	if Input.is_key_pressed(KEY_S):
+		$Player.stopped = not $Player.stopped
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
