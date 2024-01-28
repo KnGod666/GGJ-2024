8ea5e939196a9771a5b2a237c931c2c6faa19c5b
KnGod666 <ianrvr@estudiantes.uci.cu>
2024-01-28 05:57:29 -0500

    Signed-off-by: KnGod666 <ianrvr@estudiantes.uci.cu>

--- scripts/Map/map_padre.gd
+++ scripts/Map/map_padre.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 signal changeScene
 @export var spawns :Array
-@onready var base = $".."
+@onready var base = $"/root/Base/"
 
 func switchScene(id, spawn):
 	emit_signal("changeScene",id,spawn)
